ca20a9e05c40bb0618bf5db06b41fba508bdda9f
Jhonnatan-10 <jhonnatan.nascimento@bandtec.com.br>
2020-09-10 16:38:36 -0300

    Últimos testes

--- projeto-Individual/src/main/java/br/com/bandtec/Projeto/Individual/ClientesController.java
+++ projeto-Individual/src/main/java/br/com/bandtec/Projeto/Individual/ClientesController.java
@@ -20,33 +20,34 @@ public class ClientesController {
     }
 
     // Cadastrando Cliente Infantil
-    @PostMapping("/cadastrar/infantil")
+    @PostMapping("infantil")
     public String setCadastrarInfantil(@RequestBody ClienteInfantil novoCliente){
         listaClientes.add(novoCliente);
         return "Novo cliente infantil cadastrado";
     }
 
     // Cadastrando Cliente Adulto
-    @PostMapping("/cadastrar/adulto")
+    @PostMapping("adulto")
     public String setCadastrarAdulto(@RequestBody ClienteAdulto novoCliente){
         listaClientes.add(novoCliente);
         return "Novo cliente adulto cadastrado";
     }
 
     // Mostra o Clientes cadastrados através do Id
-    @GetMapping("consulta/{id}")
+    @GetMapping("{id}")
     public Cliente getListaClientesId(@PathVariable int id){
         return listaClientes.get(id-1);
     }
 
 
     // Deletando um cliente
-    @DeleteMapping("/excluir/{id}")
-    public String setExcluirCliente(@PathVariable int id){
-        listaClientes.remove(id-1);
+    @DeleteMapping("{remove}")
+    public String setExcluirCliente(@PathVariable int remove){
+        listaClientes.remove(remove-1);
 
         return "Cliente excluido";
     }
 
+    
 
 }
